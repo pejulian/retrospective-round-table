945a3ae32cfbb50ab26e20bbe7ff4e8f6bec46a6
Julian Pereira <bleushade@gmail.com>
2017-07-24 17:36:12 +0800

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,19 @@ export class AppComponent {
   _window: WindowRef;
   _wheelSpinning: boolean = false;
 
+  // View variables
+  people: Array<any> = [
+     {'fillStyle' : '#eae56f', 'text' : 'Julian'},
+     {'fillStyle' : '#89f26e', 'text' : 'Jason'},
+     {'fillStyle' : '#7de6ef', 'text' : 'Jesper'},
+     {'fillStyle' : '#e7706f', 'text' : 'Mads Brunn'},
+     {'fillStyle' : '#eae56f', 'text' : 'Mads Bakholt'},
+     {'fillStyle' : '#89f26e', 'text' : 'Ulrik Mørk'}
+  ];
+  showWinner: boolean = false;
+  person: string = '';
+  talkTime: string = '';
+
   // Wheel data
   _segments: Array<any> = [];
 
@@ -26,44 +39,43 @@ export class AppComponent {
   	this._initWheel();
   }
 
+  private addPerson(name: string, color: string) {
+  	this.people.push({
+  		fillStyle: color,
+  		text: name
+  	});
+  }
+
   private _initWheel() {
 
 
+
   	this._wheel = new this._window.nativeWindow.Winwheel({
-  		'numSegments'  : 6,         // Number of segments
-      'outerRadius'  : 212,       // The size of the wheel.
-      'centerX'      : 217,       // Used to position on the background correctly.
+  		'numSegments'  : Object.keys(this.people).length,         // Number of segments
+      'outerRadius'  : 212,       															// The size of the wheel.
+      'centerX'      : 217,       															// Used to position on the background correctly.
       'centerY'      : 219,
-      'textFontSize' : 22,        // Font size.
-      'segments'     :            // Definition of all the segments.
-      [
-         {'fillStyle' : '#eae56f', 'text' : 'Julian'},
-         {'fillStyle' : '#89f26e', 'text' : 'Jason'},
-         {'fillStyle' : '#7de6ef', 'text' : 'Jesper'},
-         {'fillStyle' : '#e7706f', 'text' : 'Mads Brunn'},
-         {'fillStyle' : '#eae56f', 'text' : 'Mads Bakholt'},
-         {'fillStyle' : '#89f26e', 'text' : 'Ulrik Mørk'}
-      ],
-      'animation' :               // Definition of the animation
+      'textFontSize' : 22,       	 															// Font size.
+      'segments'     : this.people,          	// Definition of all the segments.
+      'animation' :               						// Definition of the animation
       {
           'type'     : 'spinToStop',
-          'duration' : 8,
+          'duration' : 3,
           'spins'    : 5,
           'callbackFinished': this.alertPrize.bind(this)
       }
   	});
-
   }
 
   public alertPrize() {
 
-  	console.log(this);
+  	// console.log(this);
 
     // Get the segment indicated by the pointer on the wheel background which is at 0 degrees.
     var winningSegment = this._wheel.getIndicatedSegment();
 		
 		// Do basic alert of the segment text. You would probably want to do something more interesting with this information.
-    console.log('It is', winningSegment.text + 'time to speak!');
+    console.log('It is', winningSegment.text + '\'s time to speak!');
 
   }
 
